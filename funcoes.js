9d27b510afa4ee4c410a771d10e8146d29afc6aa
KAikiGma <Kaikigamaalborgueti@gmail.com>
2022-03-16 20:00:17 -0300

    Aula 04

---
+++ funcoes.js
@@ -0,0 +1,5 @@
+var listaFilmes = ["https://br.web.img3.acsta.net/pictures/19/04/26/17/30/2428965.jpg","https://uploads.metropoles.com/wp-content/uploads/2021/11/15143747/capa-187.jpg","https://br.web.img3.acsta.net/pictures/210/299/21029996_20130821205722213.jpg"]
+
+for (var i = 0; i < listaFilmes.length; i++) {
+    document.write("<img src=" + listaFilmes[i] + ">")
+}
